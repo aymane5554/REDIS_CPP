299bd09b8e5ed3ba7fe4c42bcc708851152de7cf
aymane <aymane.arrouji@gmail.com>
2026-05-12 16:48:05 +0100

    remove open/read/write from serialize.cpp

--- client.py
+++ client.py
@@ -2,11 +2,7 @@
 import socket
 import sys
 
-HOST = '127.0.0.1'
-PORT = 8080
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-def Lpush(key, *value):
+def Lpush(key, *value, s):
     message = f"*{len(value) + 2}\r\n$5\r\nLPUSH\r\n${len(key)}\r\n{key}\r\n".encode('utf-8')
     for v in value:
         message += f"${len(v)}\r\n{v}\r\n".encode('utf-8')
@@ -15,7 +11,7 @@ def Lpush(key, *value):
     print(f"LPUSH {key} {data.decode('utf-8')}")
     return data
 
-def Rpush(key, *value):
+def Rpush(key, *value, s):
     message = f"*{len(value) + 2}\r\n$5\r\nRPUSH\r\n${len(key)}\r\n{key}\r\n".encode('utf-8')
     for v in value:
         message += f"${len(v)}\r\n{v}\r\n".encode('utf-8')
@@ -24,97 +20,97 @@ def Rpush(key, *value):
     print(f"RPUSH {key} {data.decode('utf-8')}")
     return data
 
-def Lrange(key, start, stop):
+def Lrange(key, start, stop, s):
     message = f"*4\r\n$6\r\nLRANGE\r\n${len(key)}\r\n{key}\r\n${len(start)}\r\n{start}\r\n${len(stop)}\r\n{stop}\r\n".encode('utf-8')
     s.send(message)
     data = s.recv(1024)
     print(f"Lrange {key} {data.decode('utf-8')}")
     return data
 
-def Hset(key, field, value):
+def Hset(key, field, value, s):
     message = f"*4\r\n$4\r\nHSET\r\n${len(key)}\r\n{key}\r\n${len(field)}\r\n{field}\r\n${len(value)}\r\n{value}\r\n".encode('utf-8')
     s.send(message)
     data = s.recv(1024)
     print(f"HSET {key} {field} {data.decode('utf-8')}")
     return data
 
-def Hget(key, field):
+def Hget(key, field, s):
     message = f"*3\r\n$4\r\nHGET\r\n${len(key)}\r\n{key}\r\n${len(field)}\r\n{field}\r\n".encode('utf-8')
     s.send(message)
     data = s.recv(1024)
     print(f"HGET {key} {field} {data.decode('utf-8')}")
     return data
 
-def Hgetall(key):
+def Hgetall(key, s):
     message = f"*2\r\n$7\r\nHGETALL\r\n${len(key)}\r\n{key}\r\n".encode('utf-8')
     s.send(message)
     data = s.recv(1024)
     print(f"HGETALL {key} {data.decode('utf-8')}")
     return data
 
-def Hdel(key, field):
+def Hdel(key, field, s):
     message = f"*3\r\n$4\r\nHDEL\r\n${len(key)}\r\n{key}\r\n${len(field)}\r\n{field}\r\n".encode('utf-8')
     s.send(message)
     data = s.recv(1024)
     print(f"HDEL {key} {field} {data.decode('utf-8')}")
     return data
 
-def Set(key, value):
+def Set(key, value, s):
     message = f"*3\r\n$3\r\nSET\r\n${len(key)}\r\n{key}\r\n${len(value)}\r\n{value}\r\n".encode('utf-8')
     s.send(message)
     data = s.recv(1024)
     print(f"SET {key} {value} {data.decode('utf-8')}")
     return data
 
-def Get(key):
+def Get(key, s):
     message = f"*2\r\n$3\r\nGET\r\n${len(key)}\r\n{key}\r\n".encode('utf-8')
     s.send(message)
     data = s.recv(1024)
     print(f"GET {key} {data.decode('utf-8')}")
 
-def Expire(key, seconds):
+def Expire(key, seconds, s):
     message = f"*3\r\n$6\r\nEXPIRE\r\n${len(key)}\r\n{key}\r\n${len(str(seconds))}\r\n{seconds}\r\n".encode('utf-8')
     s.send(message)
     data = s.recv(1024)
     print(f"EXPIRE {key} {seconds} {data.decode('utf-8')}")
 
-def TTL(key):
+def TTL(key, s):
     message = f"*2\r\n$3\r\nTTL\r\n${len(key)}\r\n{key}\r\n".encode('utf-8')
     s.send(message)
     data = s.recv(1024)
     print(f"TTL {key} {data.decode('utf-8')}")
 
-def Del(key):
+def Del(key, s):
     message = f"*2\r\n$3\r\nDEL\r\n${len(key)}\r\n{key}\r\n".encode('utf-8')
     s.send(message)
     data = s.recv(1024)
     print(f"DEL {key} {data.decode('utf-8')}")
 
-def Lpop(key):
+def Lpop(key, s):
     message = f"*2\r\n$4\r\nLPOP\r\n${len(key)}\r\n{key}\r\n".encode('utf-8')
     s.send(message)
     data = s.recv(1024)
     print(f"LPOP {key} {data.decode('utf-8')}")
 
-def Rpop(key):
+def Rpop(key, s):
     message = f"*2\r\n$4\r\nRPOP\r\n${len(key)}\r\n{key}\r\n".encode('utf-8')
     s.send(message)
     data = s.recv(1024)
     print(f"RPOP {key} {data.decode('utf-8')}")
 
-def Exists(key):
+def Exists(key, s):
     message = f"*2\r\n$6\r\nEXISTS\r\n${len(key)}\r\n{key}\r\n".encode('utf-8')
     s.send(message)
     data = s.recv(1024)
     print(f"EXISTS {key} {data.decode('utf-8')}")
 
-def Flush():
+def Flush(s):
     message = b"*1\r\n$5\r\nFLUSH\r\n"
     s.send(message)
     data = s.recv(1024)
     print(f"FLUSH {data.decode('utf-8')}")
 
-def Lru():
+def Lru(s):
     message = b"*1\r\n$3\r\nLRU\r\n"
     s.send(message)
     data = s.recv(1024)
@@ -187,9 +183,12 @@ def main():
 
     if cmd == "expire":
         args[1] = int(args[1])
-
+    
+    HOST = '127.0.0.1'
+    PORT = 8080
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((HOST, PORT))
-    fn(*args)
+    fn(*args, s=s)
     s.close()
 
 if __name__ == "__main__":
--- src/Serialize.cpp
+++ src/Serialize.cpp
@@ -41,17 +41,14 @@ bool hashed(std::unordered_map<str, str> *hash, char *field, char *value)
     return true;
 }
 
-void read_wrapper(int fd, void *buf, size_t count)
+void read_wrapper(std::ifstream &ifs, void *buf, size_t count)
 {
-    ssize_t bytes_read = 0;
-
-    bytes_read = read(fd, buf, count);
-    if (bytes_read <= 0)
+    ifs.read(static_cast<char *>(buf), count);
+    if (ifs.gcount() != static_cast<std::streamsize>(count))
     {
-        perror("Read error");
+        perror("Deserialization Failed");
         exit(1);
     }
-
 }
 
 void Cache::Deserialize(const str &db_file)
@@ -59,7 +56,6 @@ void Cache::Deserialize(const str &db_file)
     char buff[8];
     const char *buff2 = "\x00\x46\x54\x52\x45\x44\x49\x53";
     int keys_num = 0;
-    int fd = 0;
     int len = 0;
     int klen = 0;
     u_char is_ttl = 0;
@@ -74,46 +70,45 @@ void Cache::Deserialize(const str &db_file)
         std::cout << "costumd.db does not exist" << std::endl;
         return;
     }
-    fd = open(db_file.c_str(), O_RDONLY);
-    if (fd < 0)
+    std::ifstream ifs(db_file, std::ios::binary);
+    if (!ifs)
     {
-        perror("Serialization Failed");
-        exit(1);
+        perror("Deserialization Failed");
         return ;
     }
-    read_wrapper(fd, buff, 8);
+    read_wrapper(ifs, buff, 8);
     if (memcmp(buff, buff2, 8))
     {
         perror("Unvalid file format");
         return ;
     }
-    read_wrapper(fd, &keys_num, 4);
+    read_wrapper(ifs, &keys_num, 4);
     for (int i = 0; i < keys_num; i++)
     {
         obj.seconds = -1;
-        read_wrapper(fd, &is_ttl, 1);
+        read_wrapper(ifs, &is_ttl, 1);
         if (is_ttl)
-            read_wrapper(fd, &obj.seconds, 8);
-        read_wrapper(fd, &obj.type, 1);
-        read_wrapper(fd, &klen, 4);
+            read_wrapper(ifs, &obj.seconds, 8);
+        read_wrapper(ifs, &obj.type, 1);
+        read_wrapper(ifs, &klen, 4);
         key = new (std::nothrow) char[klen + 1];
         while (key == NULL)
         {
             LRU();
             key = new (std::nothrow) char[klen + 1];
         }
-        read_wrapper(fd, key, klen);
+        read_wrapper(ifs, key, klen);
         key[klen] = '\0';
         if (obj.type == Val::STR)
         {
-            read_wrapper(fd, &len, 4);
+            read_wrapper(ifs, &len, 4);
             value = new (std::nothrow) char[len + 1];
             while (value == NULL)
             {
                 LRU();
                 value = new (std::nothrow) char[len + 1];
             }
-            read_wrapper(fd, value, len);
+            read_wrapper(ifs, value, len);
             value[len] = '\0';
             obj.ptr = new (std::nothrow) str(value);
             while (obj.ptr == NULL)
@@ -143,17 +138,17 @@ void Cache::Deserialize(const str &db_file)
                 LRU();
                 obj.ptr = new (std::nothrow) std::deque<str>;
             }
-            read_wrapper(fd, &li_size, 4);
+            read_wrapper(ifs, &li_size, 4);
             for (int li = 0; li < li_size; li++)
             {
-                read_wrapper(fd, &li_val_len, 4);
+                read_wrapper(ifs, &li_val_len, 4);
                 li_ptr = new (std::nothrow) char[li_val_len + 1];
                 while (li_ptr == NULL)
                 {
                     LRU();
                     li_ptr = new (std::nothrow) char[li_val_len + 1];
                 }
-                read_wrapper(fd, li_ptr, li_val_len);
+                read_wrapper(ifs, li_ptr, li_val_len);
                 li_ptr[li_val_len] = '\0';
                 while (!pushed(static_cast<std::deque<str> *>(obj.ptr), li_ptr))
                 {
@@ -184,27 +179,27 @@ void Cache::Deserialize(const str &db_file)
                 LRU();
                 obj.ptr = new (std::nothrow) std::unordered_map<str, str>;
             }
-            read_wrapper(fd, &fields_size, 4);
+            read_wrapper(ifs, &fields_size, 4);
             for (int h = 0; h < fields_size; h++)
             {
-                read_wrapper(fd, &field_len, 4);
+                read_wrapper(ifs, &field_len, 4);
                 field_ptr = new (std::nothrow) char[field_len + 1];
                 while (field_ptr == NULL)
                 {
                     LRU();
                     field_ptr = new (std::nothrow) char[field_len + 1];
                 }
-                read_wrapper(fd, field_ptr, field_len);
+                read_wrapper(ifs, field_ptr, field_len);
                 field_ptr[field_len] = '\0';
 
-                read_wrapper(fd, &value_len, 4);
+                read_wrapper(ifs, &value_len, 4);
                 value_ptr = new (std::nothrow) char[value_len + 1];
                 while (value_ptr == NULL)
                 {
                     LRU();
                     value_ptr = new (std::nothrow) char[value_len + 1];
                 }
-                read_wrapper(fd, value_ptr, value_len);
+                read_wrapper(ifs, value_ptr, value_len);
                 value_ptr[value_len] = '\0';
                 while (!hashed(static_cast<std::unordered_map<str, str> *>(obj.ptr), field_ptr, value_ptr))
                 {
@@ -230,16 +225,14 @@ void Cache::Deserialize(const str &db_file)
         }
         delete[] key;
     }
-    close(fd);
     std::cout << "End of Deserialization" << std::endl;
 }
 
 void Cache::Serialize(const str &db_file)
 {
     std::cout << "Serialize" << std::endl;
-    int fd = open(db_file.c_str(), O_CREAT | O_TRUNC | O_RDWR, 0777);
-    if (fd < 0)
-    {
+    std::ofstream ofs(db_file, std::ios::binary | std::ios::trunc);
+    if (!ofs)    {
         perror("Serialization Failed");
         return ;
     }
@@ -249,58 +242,56 @@ void Cache::Serialize(const str &db_file)
     str *s_val;
     Val *val;
 
-    write (fd, MAGIC_NUMBER, 8);
-    write (fd, &keys_number, 4);
+    ofs.write(MAGIC_NUMBER, 8);
+    ofs.write(reinterpret_cast<const char *>(&keys_number), 4);
     if (!front->next)
     {
-        close(fd);
         return ;
     }
     for (Val *it = front->next; it != NULL; it = it->next)
     {
         val = it;
         is_ttl = (val->seconds == -1) ? 0 : 1;
-        write (fd, &is_ttl, 1);
+        ofs.write(reinterpret_cast<const char *>(&is_ttl), 1);
         if (is_ttl)
-            write (fd, &val->seconds, 8);
-        write(fd, &val->type, 1);
+            ofs.write(reinterpret_cast<const char *>(&val->seconds), 8);
+        ofs.write(reinterpret_cast<const char *>(&val->type), 1);
         len = strlen(val->key);
-        write (fd, &len, 4);
-        write (fd, val->key, len);
+        ofs.write(reinterpret_cast<const char *>(&len), 4);
+        ofs.write(val->key, len);
         if (val->type == Val::STR)
         {
             s_val = static_cast<str *>(val->ptr);
             len = s_val->length();
-            write (fd, &len, 4);
-            write (fd, s_val->c_str(), len);
+            ofs.write(reinterpret_cast<const char *>(&len), 4);
+            ofs.write(s_val->c_str(), len);
         }
         else if (val->type == Val::LIST)
         {
             keys_number = static_cast<std::deque <str> *>(val->ptr)->size();
-            write (fd, &keys_number, 4);
+            ofs.write(reinterpret_cast<const char *>(&keys_number), 4);
             std::deque<str>::iterator end = static_cast<std::deque <str> *>(val->ptr)->end();
             for (std::deque<str>::iterator li = static_cast<std::deque <str> *>(val->ptr)->begin(); li != end; li++)
             {
                 len = li->length();
-                write (fd, &len, 4);
-                write (fd, li->c_str(), len);
+                ofs.write(reinterpret_cast<const char *>(&len), 4);
+                ofs.write(li->c_str(), len);
             }
         }
         else if (val->type == Val::HASH)
         {
             std::unordered_map<str, str> *h = static_cast<std::unordered_map<str, str> *>(val->ptr);
             keys_number = h->size();
-            write(fd, &keys_number, 4);
+            ofs.write(reinterpret_cast<const char *>(&keys_number), 4);
             for (std::unordered_map<str, str>::iterator hit = h->begin(); hit != h->end(); ++hit)
             {
                 len = hit->first.length();
-                write(fd, &len, 4);
-                write(fd, hit->first.c_str(), len);
+                ofs.write(reinterpret_cast<const char *>(&len), 4);
+                ofs.write(hit->first.c_str(), len);
                 len = hit->second.length();
-                write(fd, &len, 4);
-                write(fd, hit->second.c_str(), len);
+                ofs.write(reinterpret_cast<const char *>(&len), 4);
+                ofs.write(hit->second.c_str(), len);
             }
         }
     }
-    close(fd);
 }
